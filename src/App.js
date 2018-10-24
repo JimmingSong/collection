62b6fb9df769ab162eef8008310b2d6e0779e644
jimmy <784985660@qq.com>
2018-10-24 22:37:11 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Main from './pages/main'
+// import Main from './pages/main'
+import List from './pages/com/list'
+// import MainRoute from './route'
+import {BrowserRouter, Route,Router} from 'react-router-dom'
+import Main from "./pages/main";
+import About from "./pages/head";
+import Rooroy from "./pages/rooroy";
 
 class App extends Component {
     render() {
     return (
       <div className="App">
-          <Main/>
-        {/*<header className="App-header">*/}
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          {/*<p>*/}
-            {/*Edit <code>src/App.js</code> and save to reload.*/}
-          {/*</p>*/}
-          {/*<a*/}
-            {/*className="App-link"*/}
-            {/*href="https://reactjs.org"*/}
-            {/*target="_blank"*/}
-            {/*rel="noopener noreferrer"*/}
-          {/*>*/}
-            {/*Learn React*/}
-          {/*</a>*/}
-        {/*</header>*/}
+          <BrowserRouter>
+              <div>
+                  <List />
+                  <Route path='/main' render={()=><Main/>}/>
+                  <Route path='/about' render={()=><About/>}/>
+                  <Route path='/rooroy' render={()=><Rooroy/>}/>
+              </div>
+          </BrowserRouter>
       </div>
     );
   }
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Head from './head'
+// import { BrowserRouter } from 'react-router-dom'
 
 class main extends Component {
     // constructor(props) {
@@ -6,14 +8,16 @@ class main extends Component {
     // }
     render() {
         let Img = [];
-        for (var i = 1; i < 62; i++) {
-            let num = 2.000+i/1000;
-            let tag = <img src={require(`./image/作品集 ${num.toFixed(3)}.jpeg`)} key={i}/>;
-                Img.push(tag);
-        }
+        // for (var i = 1; i < 62; i++) {
+        //     let num = 2.000+i/1000;
+        //     let tag = <img src={require(`./image/作品集 ${num.toFixed(3)}.jpeg`)} key={i}/>;
+        //         Img.push(tag);
+        // }
         return(
             <div>
-                {Img}
+                {/*<Head />*/}
+                <span>main</span>
+                {/*{Img}*/}
             </div>
         )
     }
