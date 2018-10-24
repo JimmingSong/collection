0053384ab11ea562bbbc92e3cc45bc02b6d0958a
wb_songjimin <wb_songjimin@zhongan.com>
2018-10-24 18:27:39 +0800

    img

---
+++ src/pages/main.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class main extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render() {
+        let Img = [];
+        for (var i = 1; i < 62; i++) {
+            let num = 2.000+i/1000;
+            let tag = <img src={require(`./image/作品集 ${num.toFixed(3)}.jpeg`)} key={i}/>;
+                Img.push(tag);
+        }
+        return(
+            <div>
+                {Img}
+            </div>
+        )
+    }
+}
+
+export default main;
