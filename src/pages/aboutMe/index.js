1d125b55d4e4fc693cd563b9b0802038c8b6d2c8
wb_songjimin <wb_songjimin@zhongan.com>
2018-11-23 16:35:55 +0800

    part list

--- src/pages/aboutMe/index.js
+++ src/pages/aboutMe/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Icon,Divider} from 'antd'
+import {Icon, Divider} from 'antd'
 import axios from 'axios'
 import '../../css/aboutMe.css'
 
@@ -7,36 +7,36 @@ class index extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            num:1,
-            infor:{
-                name:'罗英',
-                gender:'女',
-                yearsOld:'25',
-                province:'安徽'
+            num: 1,
+            infor: {
+                name: '罗英',
+                gender: '女',
+                age: '25',
+                province: '安徽'
             },
-            contact:{
-                tel:'18355176012',
-                email:'rosylying@163.com'
+            contact: {
+                tel: '18355176012',
+                email: 'rosylying@163.com'
             },
-            infoList:[
-                {txt:'两年设计行业工作经验,能够独立完成移动端、网站等视觉设计类工作'},
-                {txt:'在创业公司工作过经历了产品从无到有的全过程,熟悉各平台设计规范,能够设计出简单易用富有设计感的UI界面,给出有效的交互解决方案'},
-                {txt:'关注设计行业发展趋势,注重产品用户体验,有良好的审美基础,能够理解目标用户的产品需求'},
-                {txt:'良好的团队意识和沟通能力,能承受一定工作压力,做事认真负责'},
-                {txt:'熟练掌握PS,AI,Sketch等设计相关软件,能通过AE,Principle等制作交互动效'},
+            infoList: [
+                {txt: '两年设计行业工作经验,能够独立完成移动端、网站等视觉设计类工作'},
+                {txt: '在创业公司工作过经历了产品从无到有的全过程,熟悉各平台设计规范,能够设计出简单易用富有设计感的UI界面,给出有效的交互解决方案'},
+                {txt: '关注设计行业发展趋势,注重产品用户体验,有良好的审美基础,能够理解目标用户的产品需求'},
+                {txt: '良好的团队意识和沟通能力,能承受一定工作压力,做事认真负责'},
+                {txt: '熟练掌握PS,AI,Sketch等设计相关软件,能通过AE,Principle等制作交互动效'},
             ],
-            skill:['PS','AI','AE','SK','C4D','AX','ID']
+            skill: ['PS', 'AI', 'AE', 'SK', 'C4D', 'AX', 'ID']
         }
     }
 
     nextPage = () => {
         this.props.history.push({
-            pathname:'part-list'
+            pathname: 'part-list'
         })
     }
 
     render() {
-        let {infor,contact,infoList,skill} = this.state;
+        let {infor, contact, infoList, skill} = this.state;
         return (
             <div className='about-box'>
                 <div className='ab-top'>
@@ -55,17 +55,17 @@ class index extends Component {
                         <div className='info-box'>
                             <div className='info-con'>information</div>
                             <div className='info-con'>{infor.name}</div>
-                            <Divider type='vertical' className='divider' />
+                            <Divider type='vertical' className='divider'/>
                             <div>
                                 <span>{infor.gender} </span>
-                                <span> {infor.yearsOld}岁</span>
+                                <span> {infor.age}岁</span>
                             </div>
                             <div>{infor.province}</div>
                         </div>
                         <div className='info-box'>
                             <div className='info-con'>information</div>
                             <div className='info-con'>联系方式</div>
-                            <Divider type='vertical' className='divider' />
+                            <Divider type='vertical' className='divider'/>
                             <div>
                                 <span>Tel:</span>
                                 <span> {contact.tel}</span>
@@ -78,15 +78,16 @@ class index extends Component {
                     </div>
                     <div className='info-list'>
                         <ul>
-                            {infoList && infoList.length && infoList.map((item,index)=>(<li key={index}>{item.txt}</li>))}
+                            {infoList && infoList.length && infoList.map((item, index) => (
+                                <li key={index}>{item.txt}</li>))}
                         </ul>
                     </div>
                     <div className='ab-sill'>
-                        {skill && skill.length && skill.map((item,index)=>(<span key={index}>{item}</span>))}
+                        {skill && skill.length && skill.map((item, index) => (<span key={index}>{item}</span>))}
                     </div>
                 </div>
                 <div className='ab-next-box'>
-                    <Icon type="right-circle" onClick={this.nextPage} className='animated infinite bounce' />
+                    <Icon type="right-circle" onClick={this.nextPage} className='animated infinite bounce'/>
                 </div>
             </div>
         )
--- src/pages/home/partList.js
+++ src/pages/home/partList.js
@@ -1,5 +1,5 @@
-import React,{Component} from 'react'
-import {Icon,Divider} from 'antd'
+import React, {Component} from 'react'
+import {Icon, Divider} from 'antd'
 import leftImg from '../../image/head/img2.003.jpeg'
 import '../../css/partList.css'
 
@@ -8,9 +8,21 @@ class PartList extends Component {
         super(props);
         this.state = {
             partData: [
-                {title:'Part 01',project:'罗欧罗意项目',list:[{txt:'ROOROY APP'},{txt:'ROOROY 企业站'},{txt:'ROOROY VIS'},{txt:'ROOROY H5'}]},
-                {title:'Part 02',project:'善诊项目',list:[{txt:'善诊 APP'},{txt:'善诊 企业站'},{txt:'善诊 H5'}]},
-                {title:'Part 03',project:'其他',list:[{txt:'AKZEN APP'},{txt:'AKZEN 企业站'},{txt:'海报'},{txt:'产品包装'},{txt:'图标等'}]},
+                {
+                    title: 'Part 01',
+                    project: '罗欧罗意项目',
+                    list: [{txt: 'ROOROY APP'}, {txt: 'ROOROY 企业站'}, {txt: 'ROOROY VIS'}, {txt: 'ROOROY H5'}]
+                },
+                {
+                    title: 'Part 02',
+                    project: '善诊项目',
+                    list: [{txt: '善诊 APP'}, {txt: '善诊 企业站'}, {txt: '善诊 H5'}]
+                },
+                {
+                    title: 'Part 03',
+                    project: '其他',
+                    list: [{txt: 'AKZEN APP'}, {txt: 'AKZEN 企业站'}, {txt: '海报'}, {txt: '产品包装'}, {txt: '图标等'}]
+                },
             ]
         }
     };
@@ -24,12 +36,14 @@ class PartList extends Component {
                         <h1>CONTENT</h1>
                     </div>
                     <div>
-                        {partData && partData.length && partData.map((item,index)=><div key={index} className='part-box'>
-                            <h3><span>{item.title}</span><Icon type="plus" /></h3>
+                        {partData && partData.length && partData.map((item, index) => <div key={index}
+                                                                                           className='part-box'>
+                            <h3><span>{item.title}</span><Icon type="plus"/></h3>
                             <Divider className='part-small-divider'/>
-                            <Divider className='part-divider h-5px' />
+                            <Divider className='part-divider h-5px'/>
                             <h2 className='part-project'>{item.project}</h2>
-                            <div>{item.list.map((det,dex)=><div key={dex} className='pd-5-0px'><span>{dex+1}. {det.txt}</span></div>)}</div>
+                            <div>{item.list.map((det, dex) => <div key={dex} className='pd-5-0px'>
+                                <span>{dex + 1}. {det.txt}</span></div>)}</div>
                         </div>)}
                     </div>
                 </div>
